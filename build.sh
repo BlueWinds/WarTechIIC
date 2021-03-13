74958b7af9cbf61e5eb3ce3c9b72ec1652b598d1
BlueWinds <blue@everblue.info>
2021-03-13 09:49:31 -0800

    Clean up folder names, create zip as part of build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 set -o errexit
 
 export BTPATH=../BATTLETECH/game
+dir=$(pwd)
 
 RED='\033[0;31m'
 NC='\033[0m' # No Color
@@ -13,4 +14,4 @@ then
     exit
 fi
 
-nodemon -x msbuild -w src/ -e .
+nodemon -x "msbuild && cd '$BTPATH/Mods/WarTechIIC' && rm -f 'WarTechIIC.log' && rm -f WIIC_systemControl.json && zip -rq '$PWD/WarTechIIC.zip' ." -w src/ -e .
--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WarTech IIC")]
+[assembly: AssemblyTitle("WarTechIIC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
